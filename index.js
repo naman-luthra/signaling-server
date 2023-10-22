7e24c95242a18898fd8d4e46422e1964f40c1b02
Naman Luthra <f20201682@pilani.bits-pilani.ac.in>
2023-10-23 02:16:34 +0530

    add dynamic port

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { createServer } from 'node:http';
+import http from 'http';
 import { Server } from 'socket.io';
 
 const app = express();
-const server = createServer(app);
+const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
       origin: "*",
@@ -84,6 +84,8 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(8000, () => {
+const PORT = process.env.PORT || 8080;
+
+server.listen(PORT, () => {
   console.log('server running at http://localhost:8000');
 });
